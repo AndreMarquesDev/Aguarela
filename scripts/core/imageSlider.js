3c960cb676362112d714c53ee23907df1c0b3962
André Marques <andregm_7@hotmail.com>
2018-11-16 00:51:03 +0000

    ADD: Images and Videos + UPD: slider changes (unstable)

--- scripts/core/imageSlider.js
+++ scripts/core/imageSlider.js
@@ -7,26 +7,57 @@ Aguarela.imageSlider = function () {
     init: function init(element) {
       var view = this;
       view.el = $(element);
-      view.variables();
-      view.events();
+      view.ele = element;
       view.initSlider();
-    },
-    variables: function variables() {
-      var view = this;
-      view.sliderContainer = view.el.find('.imageSlider__sliderContainer');
-    },
-    events: function events() {
-      var view = this; //view.menuIcon.addEventListener('click', view.initSlider.bind(view));
+      view.teste();
     },
     initSlider: function initSlider() {
       var view = this;
+      view.sliderContainer = view.el.find('.imageSlider__sliderContainer');
+      view.sliderContainer.on('init', function () {
+        // Autoplay if video is the active slide
+        !!view.el.find('.slick-current video').length && window.innerWidth > 767 && view.el.find('.slick-current video')[0].play();
+      });
       view.sliderContainer.slick({
         slidesToShow: 3,
         centerMode: true,
-        // centerPadding: '300px'
-        // adaptiveHeight: true
         variableWidth: true
+      }).on('beforeChange', function () {
+        !!view.el.find('.slick-current video').length && view.el.find('.slick-current video')[0].pause();
+      }).on('afterChange', function () {
+        !!view.el.find('.slick-current video').length && window.innerWidth > 767 && view.el.find('.slick-current video')[0].play();
       });
+    },
+    teste: function teste() {
+      var view = this;
+      var coiso;
+      !!view.ele.querySelector('.coiso') && (coiso = view.ele.querySelector('.coiso'));
+
+      var isElementInViewport = function isElementInViewport(el) {
+        var elViewportPosition = el.getBoundingClientRect();
+        return elViewportPosition.top >= 0 && elViewportPosition.left >= 0 && elViewportPosition.bottom <= (window.innerHeight || document.documentElement.clientHeight) && elViewportPosition.right <= (window.innerWidth || document.documentElement.clientWidth);
+      };
+
+      var onVisibilityChange = function onVisibilityChange(el, callback) {
+        var visible;
+        return function () {
+          if (isElementInViewport(el) !== visible) {
+            visible = isElementInViewport(el);
+            typeof callback == 'function' && callback();
+          }
+        };
+      };
+
+      var isVisible = onVisibilityChange(coiso, function () {
+        console.log('ta?');
+      });
+
+      if (window.addEventListener && !!coiso) {
+        addEventListener('DOMContentLoaded', isVisible, false);
+        addEventListener('load', isVisible, false);
+        addEventListener('scroll', isVisible, false);
+        addEventListener('resize', isVisible, false);
+      }
     }
   };
 };
--- sources/modules/ImageSlider/imageSlider.js
+++ sources/modules/ImageSlider/imageSlider.js
@@ -7,35 +7,72 @@ Aguarela.imageSlider = (() => {
         init: function (element) {
             const view = this;
                 view.el = $(element);
-                view.variables();
-                view.events();
+                view.ele = element;
+
                 view.initSlider();
-        },
-
-        variables: function () {
-            const view = this;
-
-            view.sliderContainer = view.el.find('.imageSlider__sliderContainer');
-
-        },
-
-        events: function () {
-            const view = this;
-
-            //view.menuIcon.addEventListener('click', view.initSlider.bind(view));
-
+                view.teste();
         },
 
         initSlider: function () {
             const view = this;
 
+            view.sliderContainer = view.el.find('.imageSlider__sliderContainer');
+
+            view.sliderContainer.on('init', () => {
+                // Autoplay if video is the active slide
+                (!!view.el.find('.slick-current video').length && window.innerWidth > 767) && view.el.find('.slick-current video')[0].play()
+            });
+
             view.sliderContainer.slick({
                 slidesToShow: 3,
                 centerMode: true,
-                // centerPadding: '300px'
-                // adaptiveHeight: true
                 variableWidth: true
-            })
+            }).on('beforeChange', () => {
+                (!!view.el.find('.slick-current video').length) && view.el.find('.slick-current video')[0].pause();
+            }).on('afterChange', () => {
+                (!!view.el.find('.slick-current video').length && window.innerWidth > 767) && view.el.find('.slick-current video')[0].play();
+            });
+
+        },
+
+        teste: function () {
+            const view = this;
+            let coiso;
+
+            !!view.ele.querySelector('.coiso') && (coiso = view.ele.querySelector('.coiso'));
+
+             const isElementInViewport = el => {
+
+                const elViewportPosition = el.getBoundingClientRect();
+
+                return (
+                    elViewportPosition.top >= 0 &&
+                    elViewportPosition.left >= 0 &&
+                    elViewportPosition.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                    elViewportPosition.right <= (window.innerWidth || document.documentElement.clientWidth)
+                );
+            }
+
+            const onVisibilityChange = (el, callback) => {
+                let visible;
+                return () => {
+                    if (isElementInViewport(el) !== visible) {
+                        visible = isElementInViewport(el);
+                        (typeof callback == 'function') && callback();
+                    }
+                }
+            }
+
+            const isVisible = onVisibilityChange(coiso, () => {
+                console.log('ta?')
+            });
+
+            if (window.addEventListener && !!coiso) {
+                addEventListener('DOMContentLoaded', isVisible, false);
+                addEventListener('load', isVisible, false);
+                addEventListener('scroll', isVisible, false);
+                addEventListener('resize', isVisible, false);
+            }
 
         }
     }
