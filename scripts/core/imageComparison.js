3b48f15bf23f1bc9d08e34ac5da3f259ae38eefe
André Marques <andregm_7@hotmail.com>
2018-11-30 00:33:49 +0000

    UPD: ImageComparison remade + ImageSlider to Slider

--- scripts/core/imageComparison.js
+++ scripts/core/imageComparison.js
@@ -12,53 +12,126 @@ Aguarela.imageComparison = function () {
     },
     variables: function variables() {
       var view = this;
-      view.container = view.el.querySelectorAll('.imageComparison__container');
+      view.sliderButton = view.el.querySelectorAll('.imageComparison__sliderButton');
     },
-    initComparison: function initComparison() {
+    initComparison: function initComparison(element) {
       var view = this;
+      view.sliderButton.forEach(function (element) {
+        var sliderButtonWidth = element.getBoundingClientRect().width,
+            imageBefore = element.nextElementSibling,
+            width = imageBefore.getBoundingClientRect().width,
+            height = imageBefore.getBoundingClientRect().height; //imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, ' + height + 'px, 0px)';
+        // imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, 999px, 0px)';
 
-      var compareImages = function compareImages(img) {
-        var clicked = false; // get the width and height of the img element
-
-        var slideButton = img.previousElementSibling,
-            imageWidth = img.offsetWidth,
-            imageHeight = img.offsetHeight;
-        img.style.width = imageWidth / 2 + 'px'; // position the slider in the middle:
-
-        slideButton.style.top = imageHeight / 2 - slideButton.offsetHeight / 2 + 'px';
-        slideButton.style.left = imageWidth / 2 - slideButton.offsetWidth / 2 + 'px';
-
-        var slideReady = function slideReady(event) {
-          event.preventDefault();
-          clicked = true;
-          window.addEventListener('mousemove', slideMove);
-          window.addEventListener('touchmove', slideMove);
-        };
-
-        var slideFinish = function slideFinish() {
-          return clicked = false;
-        };
-
-        var slideMove = function slideMove(event) {
-          var cursorPosition;
-          if (!clicked) return false; // get the cursor's x position:
-
-          (event = event || window.event) && (cursorPosition = event.pageX - img.getBoundingClientRect().left - window.pageXOffset); // prevent the slider from being positioned outside the image:
-
-          cursorPosition < 0 ? cursorPosition = 0 : cursorPosition > imageWidth && (cursorPosition = imageWidth);
-          img.style.width = cursorPosition + 'px';
-          slideButton.style.left = img.offsetWidth - slideButton.offsetWidth / 2 + 'px';
-        };
-
-        slideButton.addEventListener('mousedown', slideReady);
-        slideButton.addEventListener('touchstart', slideReady);
-        window.addEventListener('mouseup', slideFinish);
-        window.addEventListener('touchstop', slideFinish);
-      };
-
-      view.el.querySelectorAll('.imageComparison__overlayedImage').forEach(function (element) {
-        return compareImages(element);
+        var mouseDownX = false,
+            X;
+        element.style.left = width / 2 - sliderButtonWidth / 2 + 'px';
+        element.addEventListener('mousedown', function (event) {
+          X = event.clientX;
+          mouseDownX = true;
+        });
+        element.addEventListener('touchstart', function (event) {
+          X = event.touches[0].clientX;
+          mouseDownX = true;
+        });
+        element.addEventListener('mouseup', function (event) {
+          return mouseDownX = false;
+        });
+        element.addEventListener('touchend', function (event) {
+          return mouseDownX = false;
+        });
+        element.addEventListener('mouseout', function (event) {
+          return mouseDownX = false;
+        });
+        element.addEventListener('mousemove', function () {
+          if (mouseDownX) {
+            this.style.left = parseInt(this.style.left) + (event.clientX - X) + 'px';
+            X = event.clientX;
+            this.nextElementSibling.style.clip = 'rect(0px, ' + (this.getBoundingClientRect().width / 2 + parseInt(this.style.left)) + 'px, ' + this.getBoundingClientRect().height + 'px, 0px)';
+          }
+        });
+        element.addEventListener('touchmove', function (event) {
+          if (mouseDownX) {
+            this.style.left = parseInt(this.style.left) + (event.touches[0].clientX - X) + 'px';
+            X = event.touches[0].clientX;
+            this.nextElementSibling.style.clip = 'rect(0px, ' + (this.getBoundingClientRect().width / 2 + parseInt(this.style.left)) + 'px, ' + this.getBoundingClientRect().height + 'px, 0px)';
+          }
+        });
       });
-    }
+      window.addEventListener('load', function (f) {
+        var sliderButtonre = document.querySelectorAll('.imageComparison__sliderButton');
+        var ii = sliderButtonre.length;
+
+        while (ii--) {
+          var sliderButtonWidth = sliderButtonre[ii].getBoundingClientRect().width;
+          var imageBefore = sliderButtonre[ii].nextElementSibling;
+          var width = imageBefore.getBoundingClientRect().width;
+          var height = imageBefore.getBoundingClientRect().height;
+          sliderButtonre[ii].style.left = width / 2 - sliderButtonWidth / 2 + 'px';
+          imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, ' + height + 'px, 0px)';
+        }
+      });
+      window.addEventListener('resize', function (f) {
+        var sliderButtonre = document.querySelectorAll('.imageComparison__sliderButton');
+        var ii = sliderButtonre.length;
+
+        while (ii--) {
+          var sliderButtonWidth = sliderButtonre[ii].getBoundingClientRect().width;
+          var imageBefore = sliderButtonre[ii].nextElementSibling;
+          var width = imageBefore.getBoundingClientRect().width;
+          var height = imageBefore.getBoundingClientRect().height;
+          sliderButtonre[ii].style.left = width / 2 - sliderButtonWidth / 2 + 'px';
+          imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, ' + height + 'px, 0px)';
+        }
+      });
+    } // init: function (element) {
+    //     const view = this;
+    //         view.el = element;
+    //         view.variables();
+    //         view.initComparison();
+    // },
+    // variables: function () {
+    //     const view = this;
+    //     view.container = view.el.querySelectorAll('.imageComparison__container');
+    // },
+    // initComparison: function () {
+    //     const view = this;
+    //     const compareImages = img => {
+    //         let clicked = false;
+    //         // get the width and height of the img element
+    //         const slideButton = img.previousElementSibling,
+    //             imageWidth = img.offsetWidth,
+    //             imageHeight = img.offsetHeight;
+    //         img.style.width = (imageWidth / 2) + 'px';
+    //         // position the slider in the middle:
+    //         slideButton.style.top = (imageHeight / 2) - (slideButton.offsetHeight / 2) + 'px';
+    //         slideButton.style.left = (imageWidth / 2) - (slideButton.offsetWidth / 2) + 'px';
+    //         const slideReady = event => {
+    //             event.preventDefault();
+    //             clicked = true;
+    //             window.addEventListener('mousemove', slideMove);
+    //             window.addEventListener('touchmove', slideMove);
+    //         }
+    //         const slideFinish = () => clicked = false;
+    //         const slideMove = event => {
+    //             let cursorPosition;
+    //             if (!clicked) return false;
+    //             // get the cursor's x position:
+    //             (event = event || window.event) && (cursorPosition = event.pageX - img.getBoundingClientRect().left - window.pageXOffset);
+    //             // prevent the slider from being positioned outside the image:
+    //             cursorPosition < 0
+    //                 ? cursorPosition = 0
+    //                 : cursorPosition > imageWidth && (cursorPosition = imageWidth);
+    //             img.style.width = cursorPosition + 'px';
+    //             slideButton.style.left = img.offsetWidth - (slideButton.offsetWidth / 2) + 'px';
+    //         }
+    //         slideButton.addEventListener('mousedown', slideReady);
+    //         slideButton.addEventListener('touchstart', slideReady);
+    //         window.addEventListener('mouseup', slideFinish);
+    //         window.addEventListener('touchstop', slideFinish);
+    //     }
+    //     view.el.querySelectorAll('.imageComparison__overlayedImage').forEach(element => compareImages(element));
+    // }
+
   };
 };
--- sources/modules/ImageComparison/imageComparison.js
+++ sources/modules/ImageComparison/imageComparison.js
@@ -15,63 +15,153 @@ Aguarela.imageComparison = (() => {
         variables: function () {
             const view = this;
 
-            view.container = view.el.querySelectorAll('.imageComparison__container');
+            view.sliderButton = view.el.querySelectorAll('.imageComparison__sliderButton');
 
         },
 
-        initComparison: function () {
+        initComparison: function (element) {
             const view = this;
 
-            const compareImages = img => {
-                let clicked = false;
+                view.sliderButton.forEach(element => {
+                    const sliderButtonWidth = element.getBoundingClientRect().width,
+                        imageBefore = element.nextElementSibling,
+                        width = imageBefore.getBoundingClientRect().width,
+                        height = imageBefore.getBoundingClientRect().height;
+                    //imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, ' + height + 'px, 0px)';
+                    // imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, 999px, 0px)';
+                    let mouseDownX = false,
+                        X;
 
-                // get the width and height of the img element
-                const slideButton = img.previousElementSibling,
-                    imageWidth = img.offsetWidth,
-                    imageHeight = img.offsetHeight;
+                    element.style.left = width / 2 - sliderButtonWidth / 2 + 'px';
 
-                img.style.width = (imageWidth / 2) + 'px';
+                    element.addEventListener('mousedown', event => {
+                        X = event.clientX;
+                        mouseDownX = true;
+                    });
+                    element.addEventListener('touchstart', event => {
+                        X = event.touches[0].clientX;
+                        mouseDownX = true;
+                    });
 
-                // position the slider in the middle:
-                slideButton.style.top = (imageHeight / 2) - (slideButton.offsetHeight / 2) + 'px';
-                slideButton.style.left = (imageWidth / 2) - (slideButton.offsetWidth / 2) + 'px';
+                    element.addEventListener('mouseup', event => mouseDownX = false);
+                    element.addEventListener('touchend', event => mouseDownX = false);
+                    element.addEventListener('mouseout', event => mouseDownX = false);
 
-                const slideReady = event => {
-                    event.preventDefault();
-                    clicked = true;
-                    window.addEventListener('mousemove', slideMove);
-                    window.addEventListener('touchmove', slideMove);
-                }
+                    element.addEventListener('mousemove', function() {
+                        if (mouseDownX) {
+                            this.style.left = parseInt(this.style.left) + (event.clientX - X) + 'px';
+                            X = event.clientX;
+                            this.nextElementSibling.style.clip = 'rect(0px, ' + (this.getBoundingClientRect().width / 2 + parseInt(this.style.left)) + 'px, ' + this.getBoundingClientRect().height + 'px, 0px)';
+                        }
+                    });
 
-                const slideFinish = () => clicked = false;
+                    element.addEventListener('touchmove', function(event) {
+                        if (mouseDownX) {
+                            this.style.left = parseInt(this.style.left) + (event.touches[0].clientX - X) + 'px';
+                            X = event.touches[0].clientX;
+                            this.nextElementSibling.style.clip = 'rect(0px, ' + (this.getBoundingClientRect().width / 2 + parseInt(this.style.left)) + 'px, ' + this.getBoundingClientRect().height + 'px, 0px)';
+                        }
+                    });
 
-                const slideMove = event => {
-                    let cursorPosition;
+                })
 
-                    if (!clicked) return false;
+                window.addEventListener('load', function(f) {
+                    var sliderButtonre = document.querySelectorAll('.imageComparison__sliderButton');
+                    var ii = sliderButtonre.length;
+                    while (ii--) {
+                        var sliderButtonWidth = sliderButtonre[ii].getBoundingClientRect().width;
+                        var imageBefore = sliderButtonre[ii].nextElementSibling;
+                        var width = imageBefore.getBoundingClientRect().width;
+                        var height = imageBefore.getBoundingClientRect().height;
+                        sliderButtonre[ii].style.left = width / 2 - sliderButtonWidth / 2 + 'px';
+                        imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, ' + height + 'px, 0px)';
+                    }
+                });
 
-                    // get the cursor's x position:
-                    (event = event || window.event) && (cursorPosition = event.pageX - img.getBoundingClientRect().left - window.pageXOffset);
 
-                    // prevent the slider from being positioned outside the image:
-                    cursorPosition < 0
-                        ? cursorPosition = 0
-                        : cursorPosition > imageWidth && (cursorPosition = imageWidth);
+                window.addEventListener('resize', function(f) {
+                    var sliderButtonre = document.querySelectorAll('.imageComparison__sliderButton');
+                    var ii = sliderButtonre.length;
+                    while (ii--) {
+                        var sliderButtonWidth = sliderButtonre[ii].getBoundingClientRect().width;
+                        var imageBefore = sliderButtonre[ii].nextElementSibling;
+                        var width = imageBefore.getBoundingClientRect().width;
+                        var height = imageBefore.getBoundingClientRect().height;
+                        sliderButtonre[ii].style.left = width / 2 - sliderButtonWidth / 2 + 'px';
+                        imageBefore.style.clip = 'rect(0px, ' + width / 2 + 'px, ' + height + 'px, 0px)';
+                    }
+                });
 
-                    img.style.width = cursorPosition + 'px';
-                    slideButton.style.left = img.offsetWidth - (slideButton.offsetWidth / 2) + 'px';
-                }
+        },
 
-                slideButton.addEventListener('mousedown', slideReady);
-                slideButton.addEventListener('touchstart', slideReady);
-                window.addEventListener('mouseup', slideFinish);
-                window.addEventListener('touchstop', slideFinish);
+        // init: function (element) {
+        //     const view = this;
+        //         view.el = element;
 
-            }
+        //         view.variables();
+        //         view.initComparison();
+        // },
 
-            view.el.querySelectorAll('.imageComparison__overlayedImage').forEach(element => compareImages(element));
+        // variables: function () {
+        //     const view = this;
 
-        }
+        //     view.container = view.el.querySelectorAll('.imageComparison__container');
+
+        // },
+
+        // initComparison: function () {
+        //     const view = this;
+
+        //     const compareImages = img => {
+        //         let clicked = false;
+
+        //         // get the width and height of the img element
+        //         const slideButton = img.previousElementSibling,
+        //             imageWidth = img.offsetWidth,
+        //             imageHeight = img.offsetHeight;
+
+        //         img.style.width = (imageWidth / 2) + 'px';
+
+        //         // position the slider in the middle:
+        //         slideButton.style.top = (imageHeight / 2) - (slideButton.offsetHeight / 2) + 'px';
+        //         slideButton.style.left = (imageWidth / 2) - (slideButton.offsetWidth / 2) + 'px';
+
+        //         const slideReady = event => {
+        //             event.preventDefault();
+        //             clicked = true;
+        //             window.addEventListener('mousemove', slideMove);
+        //             window.addEventListener('touchmove', slideMove);
+        //         }
+
+        //         const slideFinish = () => clicked = false;
+
+        //         const slideMove = event => {
+        //             let cursorPosition;
+
+        //             if (!clicked) return false;
+
+        //             // get the cursor's x position:
+        //             (event = event || window.event) && (cursorPosition = event.pageX - img.getBoundingClientRect().left - window.pageXOffset);
+
+        //             // prevent the slider from being positioned outside the image:
+        //             cursorPosition < 0
+        //                 ? cursorPosition = 0
+        //                 : cursorPosition > imageWidth && (cursorPosition = imageWidth);
+
+        //             img.style.width = cursorPosition + 'px';
+        //             slideButton.style.left = img.offsetWidth - (slideButton.offsetWidth / 2) + 'px';
+        //         }
+
+        //         slideButton.addEventListener('mousedown', slideReady);
+        //         slideButton.addEventListener('touchstart', slideReady);
+        //         window.addEventListener('mouseup', slideFinish);
+        //         window.addEventListener('touchstop', slideFinish);
+
+        //     }
+
+        //     view.el.querySelectorAll('.imageComparison__overlayedImage').forEach(element => compareImages(element));
+
+        // }
 
     }
 
