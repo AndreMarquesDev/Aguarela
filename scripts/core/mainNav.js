81dbd7762947ac9af18c1313261b767dd25b8221
André Marques <andregm_7@hotmail.com>
2018-12-04 22:38:55 +0000

    UPD: Changed Logo + Replaced locla videos with Youtube based ones + MainNav mobile icon animation

--- scripts/core/mainNav.js
+++ scripts/core/mainNav.js
@@ -7,28 +7,14 @@ Aguarela.mainNav = function () {
     init: function init(element) {
       var view = this;
       view.el = element;
-      view.variables();
       view.events();
-    },
-    variables: function variables() {
-      var view = this;
-      view.menuIcon = view.el.querySelector('.iconAguarela-menu');
     },
     events: function events() {
       var view = this;
-      view.menuIcon.addEventListener('click', view.openMenu.bind(view));
+      view.el.querySelector('.mainNav__menuIcon').addEventListener('click', view.openMenu.bind(view));
     },
     openMenu: function openMenu() {
       var view = this;
-
-      if (view.menuIcon.classList.contains('iconAguarela-menu')) {
-        view.menuIcon.classList.remove('iconAguarela-menu');
-        view.menuIcon.classList.add('iconAguarela-cross');
-      } else {
-        view.menuIcon.classList.remove('iconAguarela-cross');
-        view.menuIcon.classList.add('iconAguarela-menu');
-      }
-
       view.el.classList.toggle('open');
       document.querySelector('body').classList.toggle('noScroll');
     }
--- sources/modules/MainNav/mainNav.js
+++ sources/modules/MainNav/mainNav.js
@@ -4,37 +4,22 @@ Aguarela.mainNav = (() => {
 
     return {
 
-        init: function (element) {
+        init: function(element) {
             const view = this;
-                view.el = element;
-                view.variables();
-                view.events();
+            view.el = element;
+
+            view.events();
         },
 
-        variables: function () {
+        events: function() {
             const view = this;
 
-            view.menuIcon = view.el.querySelector('.iconAguarela-menu');
-
-        },
-
-        events: function () {
-            const view = this;
-
-            view.menuIcon.addEventListener('click', view.openMenu.bind(view));
+            view.el.querySelector('.mainNav__menuIcon').addEventListener('click', view.openMenu.bind(view));
 
         },
 
-        openMenu: function () {
+        openMenu: function() {
             const view = this;
-
-            if (view.menuIcon.classList.contains('iconAguarela-menu')) {
-                view.menuIcon.classList.remove('iconAguarela-menu');
-                view.menuIcon.classList.add('iconAguarela-cross');
-            } else {
-                view.menuIcon.classList.remove('iconAguarela-cross');
-                view.menuIcon.classList.add('iconAguarela-menu');
-            }
 
             view.el.classList.toggle('open');
             document.querySelector('body').classList.toggle('noScroll');
