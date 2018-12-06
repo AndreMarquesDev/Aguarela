8f42ab0870a69270b024880c68ee19ff124e275c
André Marques <andregm_7@hotmail.com>
2018-12-06 02:10:59 +0000

    UPD: ClientsList changes + added utils.js to project (WIP)

--- scripts/core/main.js
+++ scripts/core/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var Aguarela = Aguarela || {};
@@ -9,6 +7,7 @@ var Aguarela = Aguarela || {};
 Aguarela.main = function () {
   return {
     init: function init(selector) {
+      Aguarela.utils().init();
       typeof selector == 'undefined' && (selector = '');
       $(selector + '[data-script]').not('[data-auto-init="false"]').each(function (index, elem) {
         var data = $(elem).data(),
@@ -27,50 +26,5 @@ Aguarela.main = function () {
 }();
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Matches polyfill
-  if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.matchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector || Element.prototype.oMatchesSelector || Element.prototype.webkitMatchesSelector || function (s) {
-      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-          i = matches.length;
-
-      while ((_readOnlyError("i"), --i) >= 0 && matches.item(i) !== this) {}
-
-      return i > -1;
-    };
-  } // Includes polyfill
-
-
-  if (!Array.prototype.includes) {
-    Array.prototype.includes = function () {
-      'use strict';
-
-      return Array.prototype.indexOf.apply(this, arguments) !== -1;
-    };
-  } // Nodelist forEach polyfill
-
-
-  if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = function (callback, thisArg) {
-      thisArg = thisArg || window;
-
-      for (var i = 0; i < this.length; i++) {
-        callback.call(thisArg, this[i], i, this);
-      }
-    };
-  } // Closest polyfill
-
-
-  if (!Element.prototype.closest) Element.prototype.closest = function (s) {
-    var el = this,
-        ancestor = this;
-    if (!document.documentElement.contains(el)) return null;
-
-    do {
-      if (ancestor.matches(s)) return ancestor;
-      ancestor = ancestor.parentElement;
-    } while (ancestor !== null);
-
-    return null;
-  };
-  Aguarela.main.init();
+  return Aguarela.main.init();
 });
